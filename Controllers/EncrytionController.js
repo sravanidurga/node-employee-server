8518958cde6e5c34bbb67f023b6b6a7e899c91c9
sravanidurga <sravani.thota@landmarkit.in>
2019-04-18 22:51:10 +0530

    Added Encyption and Decryption of Files

--- Controllers/EncrytionController.js
+++ Controllers/EncrytionController.js
@@ -3,6 +3,7 @@ const middleware = require("../Middleware");
 const RouteStub = require('../Common/routestub');
 const EncryptionService = require("../Services/Encryption.service");
 const asyncWrapper = middleware.asyncWrapper;
+var multer = require('multer');
 
 //var fs = require('fs');
 
@@ -12,12 +13,31 @@ class EncryptionController extends Router {
         this.Service = new EncryptionService();
     }
 
+    
+    get imageMulter() {
+        var storage = multer.diskStorage({
+            destination: function (req, file, cb) {
+                cb(null, './ServerFiles')
+            },
+            filename: function (req, file, cb) {
+                cb(null, file.fieldname + '-' + Date.now())
+            }
+        })
+
+        var upload = multer({
+            storage: storage
+        }).array('image');
+
+        return upload;
+    }
+
+
     get services() {
         return [
             // Property Routes
             new RouteStub('GET', '/get-hash', 'getHash'),
             new RouteStub('GET', '/generate-keys', 'generateKeys'),
-            new RouteStub('POST', '/encrypt-with-public-key', 'encryptWithPublicKey'),
+            new RouteStub('POST', '/encrypt-with-public-key', 'encryptWithPublicKey','imageMulter'),
             new RouteStub('POST', '/decrypt-with-private-key', 'decryptWithPrivateKey')
          ];
     }
@@ -35,13 +55,26 @@ class EncryptionController extends Router {
     }
 
     async encryptWithPublicKey(req,res){
-        const data = await asyncWrapper("Failed to Encrypt", this.Service.encryptWithPublicKey,req.body.text,req.body.receiverPublicKey,req.body.senderPrivateKey);
-        console.log(data);
-        res.status(data.statusCode).send(data);
+        // const data = await asyncWrapper("Failed to Encrypt", this.Service.encryptWithPublicKey,req.body.text,req.body.receiverPublicKey,req.body.senderPrivateKey);
+        // console.log(data);
+        // res.status(data.statusCode).send(data);
+        if (req.files) {
+            const data = await asyncWrapper("Failed to Encrypt", this.Service.encryptWithPublicKey, JSON.parse(req.body.data), req.files[0]);
+            res.status(data.statusCode).send(data);
+
+        } else {
+            const data = await asyncWrapper("Failed to Encrypt", this.Service.encryptWithPublicKey, JSON.parse(req.body.data), "");
+            res.status(data.statusCode).send(data);
+        }
     }
 
+    // async decryptWithPrivateKey(req,res){
+    //     const data = await asyncWrapper("Failed to Decrypt", this.Service.decryptWithPrivateKey,req.body.encrypted,req.body.receiverPrivateKey,req.body.senderPublicKey);
+    //     console.log(data);
+    //     res.status(data.statusCode).send(data);
+    // }
     async decryptWithPrivateKey(req,res){
-        const data = await asyncWrapper("Failed to Decrypt", this.Service.decryptWithPrivateKey,req.body.encrypted,req.body.receiverPrivateKey,req.body.senderPublicKey);
+        const data = await asyncWrapper("Failed to Decrypt", this.Service.decryptWithPrivateKey,req.body);
         console.log(data);
         res.status(data.statusCode).send(data);
     }
--- Services/Encryption.service.js
+++ Services/Encryption.service.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 const { writeFileSync } = require('fs')
 const { generateKeyPairSync } = require('crypto')
 var crypto = require("crypto");
-const assymetricCrypto = require('asymmetric-crypto') 
+const assymetricCrypto = require('asymmetric-crypto')
 var path = require("path");
 var fs = require("fs");
 const passphrase = "mySecret"
@@ -15,20 +15,74 @@ class EncryptionService {
         return hash;
 
     }
-    async generateKeys(){
+    async generateKeys() {
         const myKeyPair = assymetricCrypto.keyPair();
         const theirKeyPair = assymetricCrypto.keyPair();
-        return [myKeyPair,theirKeyPair];    
+        return [myKeyPair, theirKeyPair];
     }
 
-    async encryptWithPublicKey(text, receiverPublicKey, senderPrivateKey){
-        const encrypted = assymetricCrypto.encrypt(text, receiverPublicKey, senderPrivateKey)
-        return encrypted;    
+    // async encryptWithPublicKey(text, receiverPublicKey, senderPrivateKey){
+    //     const encrypted = assymetricCrypto.encrypt(text, receiverPublicKey, senderPrivateKey)
+    //     return encrypted;    
+    // }
+
+    async encryptWithPublicKey(data, fileData) {
+        return new Promise((resolve, reject) => {
+            let fileType = "";
+            let fileName = "";
+            fileType = fileData.mimetype;
+            fileName = fileData.filename;
+
+            fs.readFile('./ServerFiles/' + fileName, 'utf8', async function readFileCallback(err, text) {
+                if (err) {
+                    console.log(err);
+                    reject({ Message: "Failed to Encrypt" });
+                } else {
+                    const encrypted = assymetricCrypto.encrypt(text, data.receiverPublicKey, data.senderPrivateKey)
+                    // return encrypted; 
+                    fs.writeFile('./ServerFiles/' + fileName, JSON.stringify(encrypted), 'utf8', async function readFileCallback(err, data) {
+                        if (err) {
+                            console.log(err);
+                            reject({ Message: "Failed to Encrypt" });
+                        } else {
+                            console.log("Successfully written");
+                            resolve({ Message: "Successfully Encrypted",fileName:fileName });
+                        }
+                    }); // write it back 
+                }
+            });
+        });   
     }
 
-    async decryptWithPrivateKey(encrypted,receiverPrivateKey, senderPublicKey){
-        const decrypted = assymetricCrypto.decrypt(encrypted.data, encrypted.nonce, senderPublicKey, receiverPrivateKey)
-        return decrypted;    
+    // async decryptWithPrivateKey(encrypted, receiverPrivateKey, senderPublicKey) {
+    //     const decrypted = assymetricCrypto.decrypt(encrypted.data, encrypted.nonce, senderPublicKey, receiverPrivateKey)
+    //     return decrypted;
+    // }
+
+    async decryptWithPrivateKey(data) {
+        // const decrypted = assymetricCrypto.decrypt(encrypted.data, encrypted.nonce, senderPublicKey, receiverPrivateKey)
+        // return decrypted;
+        return new Promise((resolve, reject) => {
+            fs.readFile('./ServerFiles/' + data.fileName, 'utf8', async function readFileCallback(err, text) {
+                if (err) {
+                    console.log(err);
+                    reject({ Message: "Failed to Decrypt" });
+                } else {
+                    var encryptedData = JSON.parse(text);
+                    const decrypted = assymetricCrypto.decrypt(encryptedData.data, encryptedData.nonce, data.senderPublicKey, data.receiverPrivateKey)
+                    // return encrypted; 
+                    fs.writeFile('./ServerFiles/' + data.fileName+"-decrypted", JSON.stringify(decrypted), 'utf8', async function readFileCallback(err, text) {
+                        if (err) {
+                            console.log(err);
+                            reject({ Message: "Failed to Decrypt" });
+                        } else {
+                            console.log("Successfully written");
+                            resolve({ Message: "Successfully Decrypted",fileName:data.fileName+"-decrypted" });
+                        }
+                    }); // write it back 
+                }
+            });
+        });  
     }
 
 }
